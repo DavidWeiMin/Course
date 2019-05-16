16f4f24a4f7ccc9e1fc7ca58b835eec07fda7e82
DavidWeiMin <1395373646@qq.com>
2019-05-16 15:45:17 +0800

    1

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ class Column_generation():
     def __init__(self):
 
         self.W = 218
-        self.w = [81,70,68,77,33,9,86,55,74,83]
-        self.demand = [44,3,48,12,22,17,31,23,8,11]
-        # self.W = 600
-        # self.w = np.random.randint(10,200,80)
-        # self.demand = np.random.randint(10,200,80)
+        # self.w = [81,70,68,77,74,53,88,23,45,59,65,71,56,106,91,87,82,85,91]
+        self.w = [48,23,45,59,65,71,56,106,91]
+        # self.demand = [44,30,48,22,31,29,37,23,12,43,42,33,22,32,17,36,27,45,41]
+        self.demand = [37,23,12,43,42,33,22,32,17]
         self.m = len(self.w)
         self.col_num = self.m
 
@@ -21,11 +20,12 @@ class Column_generation():
         for i in range(self.m):
             self.a[i,i] = int(self.W / self.w[i])
         self.substitute_col_num = 0
+        self.add_col_num = 0
 
 
     def rlpm(self):
         model = Model('RLPM')
-        x = model.continuous_var_list(self.col_num,0,name='x')
+        x = model.continuous_var_list(len(self.a[0]),0,name='x')
         constraints = []
         for i in range(self.m):
             constraints.append(model.add_constraint(model.dot(x,self.a[i,:]) >= self.demand[i]))
@@ -34,17 +34,26 @@ class Column_generation():
         self.prime = model.solution.get_all_values()
         self.objective = model.objective_value
         self.dual = model.dual_values(constraints)
+        # print(model.reduced_costs(x))
+        # print(model.slack_values(constraints))
     
-    def substitute_col(self,col):
-        theta = np.zeros(self.m)
-        for i in range(self.m):
-            if col[i] > 0:
-                theta[i] = self.demand[i] / col[i]
-            else:
-                theta[i] = float('inf')
-        out_basis = np.argmin(theta)
-        self.a[:,out_basis] = col
-        self.substitute_col_num += 1
+    def change_col(self,col):
+        if sorted(self.prime,reverse=True)[self.m - 1] > 0:
+            # self.a = self.a[:,np.array(self.prime) > 0]
+            theta = np.zeros(self.m)
+            for i in range(self.m):
+                if col[i] > 0:
+                    theta[i] = self.demand[i] / col[i]
+                else:
+                    theta[i] = float('inf')
+            out_basis = np.argmin(theta)
+            self.a[:,out_basis] = col
+            self.substitute_col_num += 1
+            print('替换')
+        else:
+            self.a = np.c_[self.a,col]
+            self.add_col_num += 1
+            print('添加')
 
     def run(self):
         self.initial()
@@ -52,11 +61,12 @@ class Column_generation():
             self.rlpm()
             knapsack = Knapsack(self.W,self.w,self.dual)
             if knapsack.objective > 1:
-                self.substitute_col(knapsack.solution)
+                self.change_col(knapsack.solution)
             else:
-                print(self.substitute_col_num)
+                # print(self.substitute_col_num)
+                # print(self.add_col_num)
                 print(self.objective)
-                print(self.dual)
+                # print(self.dual)
                 return self.a,self.prime
 
 if __name__=='__main__':
--- test.py
+++ test.py
@@ -1,24 +1,34 @@
-from docplex.mp.model import Model
+# from docplex.mp.model import Model
+# import numpy as np
+# W = 218
+# w = [81,70,68,77,74,53,88]
+# demand = [44,30,48,22,31,29,27]
+# m = 7
+# a = np.array([[2.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0],[0., 3., 0., 0., 1., 0., 0.],[0., 0., 2., 0., 0., 0., 0.],[0., 0., 0., 1., 0., 0., 0.],[0., 0., 0., 0., 2, 0., 0.],[1., 0., 0., 1., 0., 4., 0.],[0., 0., 0., 1., 0., 0., 2.]])
+# model = Model('RLPM')
+# x = model.continuous_var_list(m,0,name='x')
+# constraints = []
+# for i in range(m):
+#     constraints.append(model.add_constraint(model.dot(x,a[i,:]) >= demand[i]))
+# model.minimize(model.sum(x))
+# model.solve()
+# prime = model.solution.get_all_values()
+# objective = model.objective_value
+# dual = model.dual_values(constraints)
+# print(prime)
+# print(objective)
 import numpy as np
-from knapsack import Knapsack
-model = Model('Column generation')
-W = 218
-w = [81,70,68]
-c = [1,1,1,1,1,1]
-a = np.array([[1,0,0,0,1,2],[0,1,0,3,0,0],[0,0,1,0,2,0]])
-rhs = [44,3,48]
-x = model.continuous_var_list(len(c),0,name='x')
-cons = []
-for i in range(len(a[:,0])):
-    cons.append(model.add_constraint(model.dot(x,a[i,:])>=rhs[i]))
-model.minimize(model.sum(x))
-model.solve()
-print(model.objective_value)
-print(model.dual_values(cons))
-print(model.solution.get_all_values())
-knapsack = Knapsack(W,w,model.dual_values(cons))
-print(knapsack.solution)
-print(knapsack.track)
+c = np.array([4,6,7])
+a = np.array([[1,2,3,4],[2,3,5,6],[3,4,6,7]])
+print(a.shape)
+b = [1,2,3]
+for i in range(len(a[0])):
+    if all(a[:,i] == c):
+        print('equal')
+        a = np.delete(a,i,axis=1)
+        break
+print(a)
+
 
 
 
